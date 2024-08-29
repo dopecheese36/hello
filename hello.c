ee027d7215245a310004a7fa854a015c8af03fe1
Yatzari Ramirez <yramirez7@huskers.unl.edu>
2024-08-29 10:20:00 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Yatzari Ramirez
+ *         yramirez7@unl.edu
+ * Date: 2024/08/26
+ *
+ * prints out name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Yatzari Ramirez\n");
+
+  return 0;
+}
